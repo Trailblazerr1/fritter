78b4b849d102699fe4d5b42a82bb28f7f8b8d78a
trailblazerr1 <kanurag078@gmail.com>
2021-05-16 10:08:11 +0530

    Update divison error

--- lib/tweet.dart
+++ lib/tweet.dart
@@ -122,7 +122,7 @@ class _TweetMediaState extends State<TweetMedia> {
 
   @override
   Widget build(BuildContext context) {
-    var largestAspectRatio = widget.media/2
+    var largestAspectRatio = widget.media
       .map((e) => ((e.sizes!.large!.w) ?? 1) / ((e.sizes!.large!.h) ?? 1))
       .reduce(math.max);
 
